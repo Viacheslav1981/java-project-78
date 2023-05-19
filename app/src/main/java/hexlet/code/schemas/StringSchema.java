d968eeed97676198d811f7fa8db78f502c116836
Slava <rukavanov_v@mail.ru>
2023-05-19 15:39:58 +0300

    checkstyle

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,6 +1,6 @@
 package hexlet.code.schemas;
 
-public class StringSchema extends BaseSchema {
+public final class StringSchema extends BaseSchema {
     private boolean checkRequired = false;
     private boolean checkMinLength = false;
     private int minLength = 0;
