a4717a021fdd38dca910697757701249f62515e6
Slava <rukavanov_v@mail.ru>
2023-05-19 16:39:40 +0300

    checkstyle

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,42 +1,19 @@
 package hexlet.code.schemas;
 
-import hexlet.code.Validator;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class BaseSchema {
-
-    public static void main(String[] args) {
-        System.out.println("test");
-
-        Validator v = new Validator();
-        MapSchema schema = v.map();
-        Map<String, BaseSchema> schemas = new HashMap<>();
-
-        schemas.put("name", v.string().required());
-        schemas.put("age", v.number().positive());
-
-       // System.out.println(schemas);
-        schema.shape(schemas);
-
-        Map<String, Object> human1 = new HashMap<>();
-        human1.put("name", "Kolya");
-        human1.put("age", 100);
-        schema.isValid(human1);
-
-    }
+public abstract class BaseSchema {
 
 
-
-    @SuppressWarnings("Checkstyle")
-    public boolean isValid(Object data) {
+   // @SuppressWarnings("Checkstyle")
+    public boolean isNotNull(Object data) {
         return !(data == null);
     }
 
+    public abstract boolean isValid(Object data);
 
 
-   // public final boolean isNotNull(Object data) {
+
+    //public  boolean isNotNull(Object data) {
    //     return !(data == null);
    // }
 
