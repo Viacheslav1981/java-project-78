909c50c57d95db93d013977b8123184ab550cde8
Slava <rukavanov_v@mail.ru>
2023-06-21 14:14:23 +0300

    refactor checkstyle

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,21 +1,21 @@
 package hexlet.code.schemas;
 
-import java.util.*;
+import java.util.LinkedList;
 import java.util.function.Predicate;
 
 public abstract class BaseSchema {
-    private final LinkedList<Predicate<Object>> CHECKS = new LinkedList<>();
+    private final LinkedList<Predicate<Object>> checks = new LinkedList<>();
 
     public abstract BaseSchema required();
 
     protected final void addChecks(Predicate<Object> check) {
-        CHECKS.add(check);
+        checks.add(check);
     }
 
 
     public final boolean isValid(Object data) {
 
-        for (Predicate<Object> check : CHECKS) {
+        for (Predicate<Object> check : checks) {
             if (!check.test(data)) {
                 return false;
             }
